3e62921575ddfd1d1b47e5b127e8fabb493dc709
Nagarajan M <NagarajanM@sulekha.net>
2016-12-14 14:44:37 +0530

    grunt file updated

--- gruntfile.js
+++ gruntfile.js
@@ -11,13 +11,34 @@ module.exports = function(grunt) {
         src: 'src/<%= pkg.name %>.js',
         dest: 'build/<%= pkg.name %>.min.js'
       }
+    },
+	jshint: {
+      files: ['gruntfile.js', 'src/*.js', 'build/*.js'],
+      options: {
+        globals: {
+          jQuery: true
+        }
+      }
+    },
+    watch: {
+	 configFiles: {
+		files: [ 'Gruntfile.js'],
+		options: {
+		  reload: true
+		}
+	  },	
+      files: ['gruntfile.js', 'src/*.js', 'build/*.js'],
+      tasks: ['uglify']
     }
+	
   });
 
   // Load the plugin that provides the "uglify" task.
   grunt.loadNpmTasks('grunt-contrib-uglify');
+  //grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-watch');  
 
   // Default task(s).
-  grunt.registerTask('default', ['uglify']);
+  grunt.registerTask('default', ['uglify','watch']);
 
 };
--- src/support.js
+++ src/support.js
@@ -1,1 +1,1 @@
-alert("Welcome to Grunt !")
+alert("data Called");
